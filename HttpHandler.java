94c3790da8319a773a7fc94c8fe2e3df8a36be1a
Nanadikar <avadhut.nanadikar@capgemini.com>
2021-04-12 09:12:55 -0400

    myfiles

---
+++ HttpHandler.java
@@ -0,0 +1,123 @@
+package ThreadPoolImpl;
+import java.io.*;
+import java.net.Socket;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+
+public class HttpHandler implements Runnable {
+    private Socket socket;
+    private File directory;
+
+
+    public HttpHandler(Socket socket, File directory) {
+        this.socket = socket;
+        this.directory=directory;
+    }
+
+    public void run() {
+        try {
+            handleRequest();
+        } catch (Exception e) {
+            System.err.println("Error Occured: " + e.getMessage());
+            try {
+                socket.close();
+                System.exit(0);
+            } catch (IOException e1) {
+                System.err.println("Error Closing socket Connection.");
+                System.exit(0);
+            }
+            System.err.println("Server is Terminating!");
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private void handleRequest() throws Exception {
+        InputStream input;
+        OutputStream output;
+        if (directory.isDirectory()) {
+            input = socket.getInputStream();
+            output = socket.getOutputStream();
+            serverRequest(input, output, directory.toString());
+            output.close();
+            input.close();
+        } else {
+            throw new Exception("src directory not present!");
+        }
+        socket.close();
+    }
+
+    /**
+     * @param input
+     * @param output
+     * @param root
+     * @throws Exception
+     */
+    private void serverRequest(InputStream input, OutputStream output, String root) throws Exception {
+        String line;
+        File resource = new File(directory+ "/Test.txt");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+
+        while ((line = bf.readLine()) != null) {
+            if (line.length() <= 0) {
+                break;
+            }
+            boolean isPost = line.startsWith("POST");
+            int contentLength = 0;
+            while (!(line = bf.readLine()).equals("")) {
+                 if (isPost) {
+                    final String contentHeader = "Content-Length: ";
+                    if (line.startsWith(contentHeader)) {
+                        contentLength = Integer.parseInt(line.substring(contentHeader.length()));
+                    }
+                }
+            }
+            StringBuilder body = new StringBuilder();
+            if (isPost) {
+                int c = 0;
+                for (int i = 0; i < contentLength; i++) {
+                    c = bf.read();
+                    body.append((char) c);
+
+                }
+            }
+            writeToTempDir(resource,body.toString());
+                  // send response
+            populateResponse(resource,output);
+            output.flush();
+            break;
+        }
+    }
+
+    private void writeToTempDir(File resource, String toString) {
+        
+    }
+
+    /**
+     * @param resource
+     * @param output
+     * @throws IOException
+     */
+    private void populateResponse(File resource, OutputStream output) throws IOException {
+        SimpleDateFormat format = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z");
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        String REQ_FOUND = "HTTP/1.0 200 OK\n";
+        String SERVER = "Server: HTTP server/0.1\n";
+        String DATE = "Date: " + format.format(new java.util.Date()) + "\n";
+        String CONTENT_TYPE = "Content-type: " + URLConnection.guessContentTypeFromName(resource.getName());
+        String LENGTH = "Content-Length: " + (resource.length()) + "\n\n";
+
+        String header = REQ_FOUND + SERVER + DATE + CONTENT_TYPE + LENGTH;
+        output.write(header.getBytes());
+
+        Files.copy(Paths.get(resource.toString()), output);
+        output.flush();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,63 @@
+package ThreadPoolImpl;
+
+import ThreadPoolImpl.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    File directory;
+    protected ExecutorService threadPool =
+            Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        try {
+            if(args.length == 2) {
+                int port= Integer.parseInt(args[0]);
+                Server server = new Server(port,args[1]);
+                server.start();
+            }
+            else{
+                System.err.println("Please provide all command parameters");
+                System.exit(0);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error occured:" + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public Server(int port,String dir) throws IOException {
+        serverSocket = new ServerSocket(port);
+        File tempDirectory = new File(System.getProperty("user.dir")+File.separator+dir);
+        if(!tempDirectory.isDirectory()){
+           tempDirectory.mkdir();
+        }
+        this.directory=tempDirectory;
+    }
+
+    /**
+     * @throws IOException
+     */
+    private void start() throws IOException {
+        while (true) {
+            Socket socket = serverSocket.accept();
+            HttpHandler connection = new HttpHandler(socket,directory);
+            this.threadPool.execute(
+                    connection);
+        }
+    }
+
+
+
+}
